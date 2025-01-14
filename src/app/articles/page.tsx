0d58a849c1b22aae61981b00a2a9101cd2ab0474
HulitosCode <heltonmatimbe8@gmail.com>
2025-01-14 14:37:54 +0200

    fix: ajustando articles

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,26 +1,26 @@
-import { ResponseProps } from "@/interfaces/articlesInterface"
+// import { ResponseProps } from "@/interfaces/articlesInterface"
 
-export default async function Articles(){
+// export default async function Articles(){
 
-    const response = await fetch('https://nestjs-backend-v9c5.onrender.com/articles')
-    const data: ResponseProps = await response.json()
+//     const response = await fetch('https://nestjs-backend-v9c5.onrender.com/articles')
+//     const data: ResponseProps = await response.json()
 
-    console.log(data)
+//     console.log(data)
 
-    return(
-        <div>
-            <h1>Todos Artigos</h1> 
+//     return(
+//         <div>
+//             <h1>Todos Artigos</h1> 
 
-            <div className="flex flex-col gap-4 mx-2">
-                {data.articles?.map(article => (
-                    <div key={article.id} className="bg-gray-400 p-4 rounded-md">
-                        <h2>{article.title}</h2>
-                        <p>{article.description}</p>
-                        <p>{article.body}</p>
-                        <p>{article.createdAt}</p>
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
-}
+//             <div className="flex flex-col gap-4 mx-2">
+//                 {data.articles?.map(article => (
+//                     <div key={article.id} className="bg-gray-400 p-4 rounded-md">
+//                         <h2>{article.title}</h2>
+//                         <p>{article.description}</p>
+//                         <p>{article.body}</p>
+//                         <p>{article.createdAt}</p>
+//                     </div>
+//                 ))}
+//             </div>
+//         </div>
+//     )
+// }
